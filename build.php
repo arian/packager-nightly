4688dfff3c02281c81bd06b9d710e6b70dc4faca
Arian <arian@aryweb.nl>
2011-02-07 17:22:56 +0100

    Validate the sha1 commit hash and add support for mootools-more (or whatever repository)

--- build.php
+++ build.php
@@ -5,14 +5,30 @@ ini_set('display_errors', 1);
 include dirname(__FILE__) . '/Builder.php';
 
 $commit = null;
+$repository = null;
+$repositories = array('mootools-core', 'mootools-more');
+
 if (isset($_POST['payload'])){
 	$data = json_decode($_POST['payload'], true);
 	$commit = $data['after'];
+	if (!preg_match('/^([0-9a-f]+)$/', $commit)) $commit = null;
+	if (isset($data['repository']) && isset($data['repository']['name'])){
+		$repository = $data['repository']['name'];
+		if (!in_array($repository, $repositories)) $repository = null;
+	}
 }
+
+if (!$repository) $repository = $repositories[0];
+
 
 Builder::$tmp = sys_get_temp_dir();
 
-$builder = new Builder('mootools/mootools-core');
-$builder->build('build/mootools-core.js', $commit);
+$builder = new Builder('mootools/' . $repository);
+$builder->build('build/' . $repository . '.js', $commit);
 
-// file_put_contents('build/log.txt', var_export(array($_GET, $_POST, $_SERVER), true));
+
+$log =  'commit: '. $commit . PHP_EOL
+	. 'repository: ' . $repository . PHP_EOL
+	. var_export(array($_POST, $_SERVER), true);
+
+file_put_contents('build/log.txt', $log);
